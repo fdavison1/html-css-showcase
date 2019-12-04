ca666e7ed93937e2e716ef5b9a0686f3f39fb0cd
fdavison1 <fred.noreply@gmail.com>
2019-12-04 15:12:24 -0700

    list of requirements

---
+++ src/components/Requirements.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Requirements.css'
+
+const Requirements = () => {
+    return (
+        <div>
+
+            <div className='requirements'>
+
+            <h2><span>Requirements</span></h2>
+            
+            <p> <span>Animation</span> keyframes, transition, transform</p>
+            <p><span>Fancy effects</span> box-shadow, gradients, text effects</p>
+            <p><span>Grid/Variables</span> variables, grid</p>
+            <p><span>Media Queries</span> media queries, media types</p>
+            <p><span>Pseudo Selectors</span> disabled, focus, hover, nth-child</p>
+            <p><span>Position</span> relative, absolute, fixed</p>
+            <p><span>Selectors</span> #, ., "", >, +</p>
+
+        </div>
+            </div>
+    )
+}
+
+export default Requirements
---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Title(){
+
+    return (
+        <div>
+            Title.jsx
+        </div>
+    )
+}
